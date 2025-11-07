1a6cc941cd083f63980047d72256c30dfebb0731
Finn Ryder <f_ryder@outlook.com>
2025-11-07 12:34:23 +0000

    tidying annotation

--- chunk_proccessing_fetch.R
+++ chunk_proccessing_fetch.R
@@ -14,14 +14,15 @@ library(progressr)
 fetch_area_5<-read.csv('area_5.csv')# smallest area for testing
 fetch_coast <- st_read("IRE.shp")
 
-# this is needed to combine all the landmasses. results will be inaccurate otherwise
+# This is needed to combine all the landmasses. results will be inaccurate otherwise
 fetch_coast <- st_union(fetch_coast)
 
-#points need to be sf object
+# Points need to be sf object
 points_sf<-st_as_sf(fetch_area_5,coords = c('long','lat'),crs=st_crs(fetch_coast))
 
-#for test running and debugging
-# visualise ----
+
+# Visualise ----
+## For test running and debugging
 ggplot() +
   geom_sf(data = fetch_coast, fill = "lightblue", color = "darkblue", alpha = 0.5) +  
   geom_sf(data = points_sf, color = "red", size = 2) +  # Points
@@ -32,7 +33,7 @@ num_intervals<-32
 bearing_step<- 360/num_intervals
 bear_seq<-seq(0,360-bearing_step,by=bearing_step)
 print(bear_seq)
-max_distance<-200000
+max_distance<-200000 ### In meters
 
 # Parallel processing ----
 ## Set up for parallel processing (multisession compatible with Windows)
@@ -41,19 +42,19 @@ plan(multisession)
 ## Define the chunk size (a chunk size of 80-120 seems to work well)
 chunk_size <- ceiling(nrow(points_sf) / 15000)
 
-## Split the ocean_points into chunks
+## Split the points into chunks
 chunks <- split(points_sf, ceiling(seq_along(1:nrow(points_sf)) / chunk_size))
 
-##change working directory to local/temp (onedrive causes problems when running for multiple days)
+## Change working directory to local/temp (onedrive causes problems when running for multiple days)
 setwd("C:/Temp")
 
-## Define the function to process each chunk and save directly to CSV
+## Define the function to process each chunk and save directly to a csv
 process_chunk <- function(chunk, bearings, fetch_coast, max_distance, append = TRUE) {
   chunk_results <- lapply(1:nrow(chunk), function(i) {
-    # Extract latitude and longitude
+    ### Extract latitude and longitude
     coords <- st_coordinates(chunk[i, ])
     
-    # Process the chunk
+    ### Process the chunk
     x <- fetch_len_multi(
       chunk[i, ], 
       bearings = bearings, 
@@ -70,10 +71,10 @@ process_chunk <- function(chunk, bearings, fetch_coast, max_distance, append = T
     return(x)
   })
   
-  # Combine all results for the current chunk
+  ### Combine all results for the current chunk
   combined_chunk <- do.call(rbind, chunk_results)
   
-  # Write combined chunk to CSV
+  ### Write combined chunk to CSV
   write.table(
     combined_chunk, 
     file = 'fetch_area_4_processed_v1.csv', 
@@ -99,4 +100,5 @@ with_progress({
     }
   )
 })
+
 
